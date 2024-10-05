5e9e053a355c8f705215d4bfcfed8b711007f70d
FAST <krinickij.v.a@gmail.com>
2024-10-05 17:50:25 +0300

    fix: route comtroller error
     Убран дубляж контроллера в роутинге

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataController;
+use App\Http\Controllers\DescriptionController;
 
 Route::get('/', function () {
     return view('Pages.index');
 });
 Route::get('/data', [DataController::class, 'index'])->name('data');
-Route::get('/description', [DataController::class, 'index'])->name('description');
+Route::get('/description', [DescriptionController::class, 'index'])->name('description');
