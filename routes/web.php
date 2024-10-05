d5f0250a25312ef7db51bba9a41a19f2b92b9a00
FAST <krinickij.v.a@gmail.com>
2024-10-05 16:36:56 +0300

    feat: base routing + base controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DataController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Pages.index');
 });
+Route::get('/data', [DataController::class, 'index'])->name('data');
+Route::get('/description', [DataController::class, 'index'])->name('description');
