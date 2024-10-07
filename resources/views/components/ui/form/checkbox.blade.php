4ba4bc4c33941c1c0292d6a9c207ee8bb0093a68
FAST <krinickij.v.a@gmail.com>
2024-10-07 21:20:16 +0300

    fix: fix checkbox error
    В компонент чекбокса добавлена проверка на наличие  входязего статуса чекбокса

--- resources/views/components/ui/form/checkbox.blade.php
+++ resources/views/components/ui/form/checkbox.blade.php
@@ -1,5 +1,5 @@
 <label class=" cursor-pointer flex flex-row items-center gap-3">
     <span class=" text-xl font-semibold">{{$title}}</span>
     
-    <input type="checkbox" name="{{$name}}" {{$checked ? 'checked' : ''}}>
+    <input type="checkbox" name="{{$name}}" {{ isset($checked) && $checked ? 'checked' : ''}}>
 </label>
