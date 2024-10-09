bd3645044e13703b3365b6a3ecda23cc7c28ad72
krinickij <krinickij.v.a@gmail.com>
2024-10-08 21:16:08 -0400

    feat: created seeder for post table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -17,6 +18,9 @@ class DatabaseSeeder extends Seeder
     {
         Tag::factory(20)->create();
         Category::factory(10)->create();
+
+        Post::factory(10)->create();
+        
         // User::factory(10)->create();
 
         // User::factory()->create([
