71dbc00a78ec11bedb461f58224ee6fc4e367e34
FAST <krinickij.v.a@gmail.com>
2024-10-11 15:54:49 +0300

    delete: deleted views foruser interfase

--- routes/web.php
+++ routes/web.php
@@ -13,38 +13,9 @@ use App\Http\Controllers\Admin\TagController as AdminTagController;
 Route::get('/', function () {
     return view('Pages.index');
 });
-
-
-
-Route::prefix('/data')->group(function(){
-    Route::get('/', [PostController::class, 'index'])->name('data');
-    Route::prefix('/post')->group(function(){
-        Route::get('/create',[PostController::class, 'create'])->name('post.create');
-        Route::post('/',[PostController::class, 'store'])->name('post.store');
-        Route::get('/{post}',[PostController::class, 'show'])->name('post.show');
-        Route::get('/{post}/edit',[PostController::class, 'edit'])->name('post.edit');
-        Route::patch('/{post}',[PostController::class, 'update'])->name('post.update');
-        Route::delete('/{post}',[PostController::class, 'destroy'])->name('post.delete');
-    });
-    Route::prefix('/categories')->group(function(){
-        Route::get('/', [CategoryController::class, 'index'])->name('categories');
-        Route::get('/create',[CategoryController::class, 'create'])->name('categories.create');
-        Route::post('/', [CategoryController::class, 'store'])->name('categories.store');
-        Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-        Route::patch('/{category}',[CategoryController::class, 'update'])->name('categories.update');
-        Route::delete('/{category}',[CategoryController::class, 'destroy'])->name('categories.delete');
-    });
-    Route::prefix('/tags')->group(function(){
-        Route::get('/',[TagController::class, 'index'])->name('tags');
-        Route::get('/create',[TagController::class, 'create'])->name('tags.create');
-        Route::post('/',[TagController::class, 'store'])->name('tags.store');
-        Route::get('/{tag}/edit', [TagController::class, 'edit'])->name('tags.edit');
-        Route::patch('/{tag}', [TagController::class , 'update'])->name('tags.update');
-        Route::delete('/{tag}',[TagController::class, 'destroy'])->name('tags.delete');
-    });
-   
-});
-
+Route::get('/data',function(){
+    return redirect('/admin');
+})->name('data');
 
 Route::get('/description', [DescriptionController::class, 'index'])->name('description');
 
