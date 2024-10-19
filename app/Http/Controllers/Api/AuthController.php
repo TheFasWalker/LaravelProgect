5bc7e682e2bd6835c11698d9b52f43622aeeb824
FAST <krinickij.v.a@gmail.com>
2024-10-19 18:49:08 +0300

    feat : add api route to get api token

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    public function auth(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+            'device_name' => ['required', 'string']
+        ]);    
+        
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+     
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'The provided credentials are incorrect.'], 401);
+        }
+
+        return response()->json(['token' => $user->createToken($request->device_name)->plainTextToken]);
+
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FirmController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('sanctum')->group(function() {
+    Route::post('/auth', [AuthController::class, 'auth']);
+});
 
 
 Route::resource('firm', FirmController::class);
