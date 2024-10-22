3056f393e58622acbc71efce8894dcedabb7e3a7
FAST <krinickij.v.a@gmail.com>
2024-10-22 15:14:02 +0300

    feat: edited protected routes adirted tag controller

--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -3,17 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\TagResource;
 use App\Models\Tag;
+use App\Repositories\TagRepository;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
+    private $tagRepository;
+
+    public function __construct(TagRepository $tagRepository)
+
+    {
+        $this->tagRepository = $tagRepository;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $tags = $this->tagRepository->all();
+        return TagResource::collection($tags);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\FirmController;
+use App\Http\Controllers\Api\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -13,8 +14,11 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('sanctum')->group(function() {
     Route::post('/auth', [AuthController::class, 'login']);
+});
+Route::prefix('sanctum')->middleware('auth:sanctum')->group(function() {
+   
     Route::resource('firm', FirmController::class);
     Route::resource('category',CategoryController::class);
+    Route::resource('tags',TagController::class);
 });
-
 
