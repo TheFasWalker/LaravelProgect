97b1561046dc3a32f77f18363f0ba9d100c0f787
FAST <krinickij.v.a@gmail.com>
2024-10-24 09:08:22 +0300

    feat: edited delete category action

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -70,6 +70,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category ->delete();
+        return response()->json(['message'=>'Category deleted successfully.'],200);
     }
 }
